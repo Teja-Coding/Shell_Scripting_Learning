48166a674141fe310fe186f55a92e5c8d85f568b
Teja-Coding <dropteja@gmail.com>
2025-09-21 13:31:57 +0530

    changes done buddy

--- 06_example.sh
+++ 06_example.sh
@@ -5,6 +5,9 @@ echo "i am learing shell scripting"
 echo "this is my first script"
 fristName="john"
 lastName="doe"
-echo "my name is $fristName $lastName"
+Myage=$1
+MySalarty=$2
 
 echo "my name is ${fristName} ${lastName}"
+echo "my age is ${Myage}"
+echo "my salarty is ${MySalarty}"
