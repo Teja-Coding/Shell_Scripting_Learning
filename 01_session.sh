8db8ee1d765bc6bbd54af64aff227c72a3e88b81
Teja-Coding <dropteja@gmail.com>
2025-09-26 22:29:52 +0530

    delete and created new session

--- 01_session.sh
+++ 01_session.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
-#echo is a command that outputs text to the terminal
-echo "Starting a new session..."
+# to get the comments
+echo "Starting a new session..." 
+echo "This is a sample script to demonstrate session handling."
+echo "You can add your commands here."
+echo "Session started successfully!"
+# End of script
--- 03_session.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Learning more about the varibales in bash
-echo "enter you name : "
-read name
-echo "enter your age : "
-read age
-echo "your name is $name and your age is $age"
