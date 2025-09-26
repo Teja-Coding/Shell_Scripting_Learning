f133536c074758b019b15290289db71269b9927b
Teja-Coding <dropteja@gmail.com>
2025-09-26 22:49:32 +0530

    done

---
+++ 04_user_defined.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name="Alice"
+greeting="Hello, $name! Welcome to the scripting world."
+course="shell scripting"
+duration="4 weeks"
+
+echo "$greeting"
+echo "This $course course will last for $duration."
